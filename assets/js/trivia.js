b8cada253d6afe55032c12fc4e06639d017c6d9d
FrankiePP <sp3990@nyu.edu>
2019-07-25 15:32:59 -0400

    found images for theme

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -2,9 +2,9 @@ var globalTime = 0;
 var questionTime = 0;
 var questionTimeLim = 0;
 
-var titles = ["Dreams", "domed and divided by arches","slide off","pitifully thin","a proper human"];
-var possibleQs = ["<p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin</p>","<p>He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</p>","<p>The bedding was hardly able to cover it and seemed ready to slide off any moment.</p>","<p>His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.</p>","<p>'What's happened to me?' he thought. It wasn't a dream. His room, a proper human</p>"];
-var answers = [0,0,1,1,0];
+var titles = ["Dreams", "domed and divided by arches", "slide off", "pitifully thin", "a proper human"];
+var possibleQs = ["<p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin</p>", "<p>He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</p>", "<p>The bedding was hardly able to cover it and seemed ready to slide off any moment.</p>", "<p>His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.</p>", "<p>'What's happened to me?' he thought. It wasn't a dream. His room, a proper human</p>"];
+var answers = [0, 0, 1, 1, 0];
 let alreadyAnswered = [];
 
 let currentCard;
@@ -25,9 +25,18 @@ class Question {
     cButton1;
 
 
-    constructor(){
+    constructor() {
         // var qCol = color(125,0,125);
-        this.qIndex = Math.floor(Math.random() *(possibleQs.length - 1));
+        this.qIndex = Math.floor(Math.random() * (possibleQs.length - 1));
+        if (alreadyAnswered.length >= 1) {
+            for (let thing in alreadyAnswered) {
+                if (thing === this.qIndex) {
+                    this.qIndex = possibleQs[possibleQs.lastIndexOf()];
+                }
+            }
+        }
+
+
         this.que = possibleQs[this.qIndex];
         this.ttl = titles[this.qIndex];
         this.answ = answers[this.qIndex];
@@ -51,11 +60,11 @@ class Question {
         this.cButton1.addClass("btn");
         this.cButton1.addClass("btn-primary");
 
-        this.cButton0.attr("id","zero");
-        this.cButton1.attr("id","one");
+        this.cButton0.attr("id", "zero");
+        this.cButton1.attr("id", "one");
         this.cButton0.text("True");
         this.cButton1.text("False");
-        
+
         this.cTitle.text(this.ttl);
         this.cBody.append(this.cTitle);
         this.cBody.append(this.que);
@@ -70,80 +79,58 @@ class Question {
 
 
 $(document).ready(function () {
-    
 
 
 
-    
+
+
     newQ();
 
 
-    
-    // $("#zero").on("click",function(){
-    //     console.log("here");
-    //     if(currentCard.answ === 0){
-    //         updateScore();
-    //         newQ();
-    //     } else if(currentCard.answ != 0){
-    //         alert("Wrong!");
-    //         currentCard.cButton0.removeClass("btn-prmary");
-    //         currentCard.cButton0.addClass('btn-secondary');
-    //     }
-    //  });
-    
-    //  $("#one").on("click",function(){
-    //      if(currentCard.answ === 1){
-    //          updateScore();
-    //          newQ();
-    //      } else if(currentCard.answ != 1){
-    //          alert("Wrong!");
-    //          currentCard.cButton1.removeClass("btn-prmary");
-    //          currentCard.cButton1.addClass('btn-secondary');
-    //      }
-    //  });
-
-     
 
 })
 
 
 
- function updateScore(){
+function updateScore() {
     score++;
     $("#score-txt").text("Score: " + score);
 }
 
-function newQ(){
-   $("#question").empty();
-   let nuCard = new Question();
-   currentCard = nuCard;
-   $("#question").append(nuCard.card);
-   makeGame();
+function newQ() {
+    if (currentCard != undefined) {
+        alreadyAnswered.push(currentCard.qIndex);
+    }
+    $("#question").empty();
+    let nuCard = new Question();
+    currentCard = nuCard;
+    $("#question").append(nuCard.card);
+    console.log(nuCard);
+    makeGame();
 
 }
 
-function makeGame(){
-    $("#zero").on("click",function(){
+function makeGame() {
+    $("#zero").on("click", function () {
         console.log("here");
-        if(currentCard.answ === 0){
+        if (currentCard.answ === 0) {
             updateScore();
             newQ();
-        } else if(currentCard.answ != 0){
+        } else if (currentCard.answ != 0) {
             alert("Wrong!");
             currentCard.cButton0.removeClass("btn-prmary");
             currentCard.cButton0.addClass('btn-secondary');
         }
-     });
-    
-     $("#one").on("click",function(){
-         if(currentCard.answ === 1){
-             updateScore();
-             newQ();
-         } else if(currentCard.answ != 1){
-             alert("Wrong!");
-             currentCard.cButton1.removeClass("btn-prmary");
-             currentCard.cButton1.addClass('btn-secondary');
-         }
-     });    
-}
+    });
 
+    $("#one").on("click", function () {
+        if (currentCard.answ === 1) {
+            updateScore();
+            newQ();
+        } else if (currentCard.answ != 1) {
+            alert("Wrong!");
+            currentCard.cButton1.removeClass("btn-prmary");
+            currentCard.cButton1.addClass('btn-secondary');
+        }
+    });
+}
